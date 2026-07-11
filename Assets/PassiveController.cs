99efb5170dc768b9a11b7c2a386acfd57776dec3
BaBaBuaB <arm5052sueksa@gmail.com>
2026-07-11 15:16:49 +0700

    Before ass

---
+++ Assets/PassiveController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PassiveController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public abstract class BasePassive
+{
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+}
